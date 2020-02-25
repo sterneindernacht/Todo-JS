71ce4a64cbd494fdfabf2256d171388065b985d9
sterneindernacht <kaczmarek.anita0@gmail.com>
2020-02-25 19:30:41 +0100

    added render function fo done tasks

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const todos = [
 ];
 
 const todoListContainer = document.querySelector('.todo-list');
+const doneListContainer = document.querySelector('.done-tasks');
 
 function render() {
     const htmlForUndone = todos
@@ -21,8 +22,19 @@ function render() {
             </div>
         `)
         .join('');
-
     todoListContainer.innerHTML = htmlForUndone;
+
+    const htmlForDone = todos
+        .filter(item => item.isDone)
+        .map(item => `
+             <div>
+                <h3>${item.name}</h3>
+                <input type="checkbox" ${item.isDone ? 'checked' : ''} />
+             </div>
+         `)
+        .join("");
+
+    doneListContainer.innerHTML = htmlForDone;
 }
 
-render();
+render(); 
