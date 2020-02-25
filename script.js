cd13d75a1fba358a44af1409c6ae5d2717184d28
sterneindernacht <kaczmarek.anita0@gmail.com>
2020-02-25 23:12:07 +0100

    added deleting

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const todos = [
+let todos = [
     {
+        id: "1",
         name: "Test",
         isDone: false
     },
     {
+        id: "2",
         name: "Test12313",
         isDone: true
     },
@@ -15,17 +17,19 @@ const doneListContainer = document.querySelector('.done-tasks');
 const addingButton = document.querySelector("#adding-button");
 const taskInput = document.querySelector("#taskInput");
 
+
 function render() {
     const htmlForUndone = todos
         .filter(item => !item.isDone)
         .map(item => `
-        <div class="todo-element-bar">
+        <div class="todo-element-bar" data-id="${item.id}">
             <div>
-                <h6 id="date"></h6></div>
+                <h6 id="date"></h6>
+            </div>
             <div>
                 <input class="all-tasks" type="checkbox" ${item.isDone ? 'checked' : ''} id="done-checkbox">
                 <div class="all-tasks" id="tasks-all">${item.name}</div>
-                <button class="all-tasks" id="button-delete" title="delete task">
+                <button class="all-tasks button-delete" title="delete task">
                     <i class="fas fa-trash-alt"></i>
                 </button>
             </div>
@@ -36,22 +40,32 @@ function render() {
     const htmlForDone = todos
         .filter(item => item.isDone)
         .map(item => `
-        <div class="todo-element-bar">
-        <div>
-            <h6 id="date"></h6></div>
-        <div>
-            <input class="all-tasks" type="checkbox" ${item.isDone ? 'checked' : ''} id="done-checkbox">
-            <div class="all-tasks" id="tasks-all">${item.name}</div>
-            <button class="all-tasks" id="button-delete" title="delete task">
-                <i class="fas fa-trash-alt"></i>
-            </button>
+        <div class="todo-element-bar" data-id="${item.id}">
+            <div>
+                <h6 id="date"></h6>
+            </div>
+            <div>
+                <input class="all-tasks" type="checkbox" ${item.isDone ? 'checked' : ''} id="done-checkbox">
+                <div class="all-tasks" id="tasks-all">${item.name}</div>
+                <button class="all-tasks button-delete" title="delete task">
+                    <i class="fas fa-trash-alt"></i>
+                </button>
+            </div>
         </div>
-    </div>
          `)
         .join("");
 
     todoListContainer.innerHTML = htmlForUndone;
     doneListContainer.innerHTML = htmlForDone;
+
+    document.querySelectorAll('.button-delete').forEach(node => {
+        node.addEventListener("click", function (e) {
+            e.preventDefault();
+            const idToRemove = this.closest(".todo-element-bar").getAttribute("data-id");
+            todos = todos.filter(item => item.id !== idToRemove);
+            render();
+        });
+    });
 }
 
 addingButton.addEventListener("click", function (e) {
@@ -60,12 +74,15 @@ addingButton.addEventListener("click", function (e) {
     if (taskInput.value !== "") {
         todos.push({
             name: taskInput.value,
-            isDone: false
+            isDone: false,
+            id: String(Math.random())
         });
         taskInput.value = "";
         render();
     }
 
 });
+
+
 
 render();
